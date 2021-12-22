bf9f3f64529abc41d3f39183641a2f3f614758a3
royalty37 <patu_maori@hotmail.com>
2021-12-23 02:15:22 +1300

    Install dotenv to hide Mongo details and added connection to MongoDB cluster on server

--- DietMateServer/src/app.ts
+++ DietMateServer/src/app.ts
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 3000;
+const dotenv = require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected', () => {
+  console.log("Connected to MongoDB");
+})
+mongoose.connection.on('error', error => {
+  console.error("Error connecting to MongoDB", error);
+})
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
